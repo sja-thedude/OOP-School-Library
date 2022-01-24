5716be7bef1a0ce1d3554556900901fd067e35e0
Syeda Juveria Afreen <sja.affu765@gmail.com>
2022-01-24 14:09:46 +0300

    Fixed Linter

--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require_relative 'classroom'
 
 class Student < Person
   attr_reader :classroom
-  
+
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
