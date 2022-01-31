693acef02c7856463737a88961587d399549df11
unknown <yisehakmitiku@gmail.com>
2022-02-01 00:14:38 +0800

    Update tests for classroom

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -4,7 +4,7 @@ require_relative('../student')
 describe Classroom do
   context 'It should create book and add rentals' do
     classroom = Classroom.new
-    classroom.initialize('Microverse')
+    classroom.intitialize('Microverse')
     student = Student.new(age: 27, classroom: nil, name: 'Surbhi', parent_permission: true)
     it 'should create a classroom' do
       expect(classroom.label).to eq 'Microverse'
