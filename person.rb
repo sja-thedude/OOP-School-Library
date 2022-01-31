89811bfb4251255a18e9ecee4c333b1f962cf487
Syeda Juveria Afreen <sja.affu765@gmail.com>
2022-01-31 19:09:13 +0300

    updated

--- person.rb
+++ person.rb
@@ -22,9 +22,13 @@ class Person
     @name = @corrector.correct_name(@name)
   end
 
+  private
+
   def of_age?
     @age >= 18
   end
+
+  public
 
   def can_use_services?
     of_age? || @parent_permission
